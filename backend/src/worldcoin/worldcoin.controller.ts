06f94786dc354c1e4fa369d592c878ed2d9f6072
sudeepgowda55 <ssudeepgowda55@gmail.com>
2025-02-06 19:25:05 +0530

    add nillion secret vault to store ai computed data

--- backend/src/worldcoin/worldcoin.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('worldcoin')
-export class WorldcoinController {
-  @Get()
-  async getWorldcoin(): Promise<string> {
-    return 'This is the worldcoin route';
-  }
-}
--- backend/src/worldcoin/worldcoin.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WorldcoinController } from './worldcoin.controller';
-import { WorldcoinService } from './worldcoin.service';
-
-@Module({
-  controllers: [WorldcoinController],
-  providers: [WorldcoinService]
-})
-export class WorldcoinModule {}
