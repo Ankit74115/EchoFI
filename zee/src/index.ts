b1e0a87ba40388c34a8a2f260e3e4e0410d1fa09
sudeepgowda55 <ssudeepgowda55@gmail.com>
2025-02-07 16:57:13 +0530

    added heroku config file for zee

--- zee/src/index.ts
+++ zee/src/index.ts
@@ -7,8 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(8000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 app.get("/", (req, res) => {
